c625d00a975ba3a58c06692c6ee5478ff4d541a2
SharkBee80 <115618339+SharkBee80@users.noreply.github.com>
2026-04-16 20:29:57 +0800

    update jwt.ts

--- typescript/jwt.ts
+++ typescript/jwt.ts
@@ -1,17 +1,22 @@
 //@ts-nocheck
-import jwt from "jsonwebtoken";
 
-/**
- * 签发 Token (生成)
- */
-jwt.sign(payload, key, options);
+import * as jose from "jose";
 
-/**
- * 校验 Token (验证)
- */
-jwt.verify(token, key, options);
+// node environment
+// import jwt from "jsonwebtoken";
 
-/**
- * 解析 Token (仅解码，不校验合法性)
- */
-jwt.decode(token);
+// /**
+//  * 签发 Token (生成)
+//  */
+// jwt.sign(payload, key, options);
+await new jose.SignJWT(payload).setProtectedHeader(header).sign(key);
+// /**
+//  * 校验 Token (验证)
+//  */
+// jwt.verify(token, key, options);
+await jose.jwtVerify(token, key, options);
+// /**
+//  * 解析 Token (仅解码，不校验合法性)
+//  */
+// jwt.decode(token);
+jose.decodeJwt(token);
